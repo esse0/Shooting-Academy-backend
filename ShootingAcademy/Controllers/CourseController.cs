ff5a681182448190fd591564f33be4bf7a11b119
roodnori <sinister.sobakin@gmail.com>
2025-02-23 16:58:23 +0300

    feature/pizdus indus

--- ShootingAcademy/Controllers/CourseController.cs
+++ ShootingAcademy/Controllers/CourseController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShootingAcademy.Models.DB.ModelCourse;
 
 namespace ShootingAcademy.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class CourseController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+        private readonly ICourseService _courseService;
+
+        public CourseController(IConfiguration configuration, ICourseService courseService)
+        {
+            _configuration = configuration;
+            _courseService = courseService;
+        }
     }
 }
