f83e874be3fe06e208506b045be2906737d13545
DioMain <dima123.zample@gmail.com>
2025-02-23 21:31:32 +0300

    BREAKING_CHANGES: GG

---
+++ ShootingAcademy/Models/DB/ModelUser/DTO/FullUserModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShootingAcademy.Models.DB.ModelUser.DTO
+{
+    public class FullUserModel
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string SecoundName { get; set; }
+        public string PatronymicName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Grade { get; set; }
+        public string Country { get; set; }
+
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+    }
+}
