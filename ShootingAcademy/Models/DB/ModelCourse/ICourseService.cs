593aa5d0694c189e20abf31fd376c91bdc48e6e3
roodnori <sinister.sobakin@gmail.com>
2025-02-23 19:04:15 +0300

    fix/abba

--- ShootingAcademy/Models/DB/ModelCourse/ICourseService.cs
+++ ShootingAcademy/Models/DB/ModelCourse/ICourseService.cs
@@ -6,7 +6,7 @@ namespace ShootingAcademy.Models.DB.ModelCourse
     public interface ICourseService
     {
         Task<Course> AddCourseAsync(CreateCourseDTO courseToCreate);
-        Task UpdateCourseAsync(UpdateCourseDTO courseToUpdate);
+        Task UpdateCourseAsync(UpdateCourseDTO courseToUpdate); 
         Task DeleteCourseAsync(Course course);
         Task<Course?> FindCourseByIdAsync(GetCourseDto getCourseDto);
         Task<IEnumerable<GetCourseDto>> GetCourseAsync();
