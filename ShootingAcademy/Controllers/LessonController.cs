1c35409a467931b1b5e7868691f32f27b37b0698
roodnori <sinister.sobakin@gmail.com>
2025-02-24 18:42:50 +0300

    feature/add LessonController

---
+++ ShootingAcademy/Controllers/LessonController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShootingAcademy.Models.Controllers.Features;
+using ShootingAcademy.Models.DB;
+using ShootingAcademy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShootingAcademy.Controllers
+{
+    public class LessonController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationDbContext _context;
+
+        public LessonController(IConfiguration configuration, ApplicationDbContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] object form)
+        {
+            try
+            {
+                IEnumerable<Feature> Users = await _context.Features.AsNoTracking().ToListAsync();
+
+                return Ok(new FeatureResponse()
+                {
+
+                });
+
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new FeatureResponse()
+                {
+                    error = error.Message,
+                });
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] object form)
+        {
+            try
+            {
+
+                //_context.Lessons.Add();
+
+                return Ok(new FeatureResponse());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new FeatureResponse()
+                {
+                    error = error.Message,
+                });
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] object form)
+        {
+            try
+            {
+
+                //_context.Lessons.Update();
+
+                return Ok(new FeatureResponse());
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new FeatureResponse()
+                {
+                    error = error.Message,
+                });
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                //_context.Lessons.Remove();
+
+                return Ok(new FeatureResponse());
+
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new FeatureResponse()
+                {
+                    error = error.Message,
+                });
+            }
+        }
+    }
+}
