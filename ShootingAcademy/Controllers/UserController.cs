4b6d5547cc8c2b198932662151b79d267fffff17
DioMain <dima123.zample@gmail.com>
2025-02-24 20:47:29 +0300

    feat: coors

---
+++ ShootingAcademy/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShootingAcademy.Models.Controllers.User;
+using ShootingAcademy.Models.Exceptions;
+
+namespace ShootingAcademy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private Guid UserGuid => Guid.Parse(HttpContext.User.Claims.ToArray()[0].Value);
+        private bool IsAutorize => HttpContext.User.Claims.Any();
+        private string Role => HttpContext.User.Claims.ToArray()[1].Value;
+
+        [HttpPut, Authorize]
+        public IResult Put([FromBody] UserProfileData profileData)
+        {
+            try
+            {
+                //profileData.
+            }
+            catch (BaseException exp)
+            {
+            }
+
+            return Results.Ok();
+        }
+    }
+}
---
+++ ShootingAcademy/Services/AutorizeDataService.cs
@@ -0,0 +1,18 @@
+﻿namespace ShootingAcademy.Services
+{
+    public class AutorizeDataService
+    {
+        private readonly HttpContent content;
+
+        //public Guid UserGuid => content.Use.Claims.ToArray()[0].Value;
+        //public bool IsAutorize => content.us.Claims.Any();
+        //public string Role => content.User.Claims.ToArray()[1].Value;
+
+        //public AutorizeDataService(HttpContent content)
+        //{
+        //    this.content = content;
+
+        //    content
+        //}
+    }
+}
